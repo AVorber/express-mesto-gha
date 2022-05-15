f63e4575be69077485ba33a892ab85dbd2137265
a.rebrov <a.rebrov@cian.ru>
2022-05-15 14:30:34 +0300

    feat: создать модели

---
+++ models/card.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  likes: [
+    {
+      type: ObjectId,
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
