73dc4425d371cfc344731afbacf6c0e2ee339f37
a.rebrov <a.rebrov@cian.ru>
2022-05-15 16:53:06 +0300

    fix: исправляет обработку несуществующей страницы

--- routes/app.js
+++ routes/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { usersRoutes } = require('./users');
 const { cardsRoutes } = require('./cards');
+const { NOT_FOUND_ERROR } = require('../controllers/errors');
 
 const routes = express.Router();
 
 routes.use('/users', usersRoutes);
 routes.use('/cards', cardsRoutes);
+routes.use((req, res, next) => {
+  next(res.status(NOT_FOUND_ERROR).json({ message: 'Страница не найдена' }));
+});
 
 module.exports = { routes };
