a414e7c6970a5ea60ac02ad8cbcfdcd57c3da31b
a.rebrov <a.rebrov@cian.ru>
2022-07-06 23:43:36 +0300

    rfg: переносит логин и создание пользователя в routes

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const { celebrate, errors, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const { routes } = require('./routes/app');
-const { isURL } = require('./helpers/regex');
-
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
+const { SERVER_ERROR } = require('./errors/constants');
 
 const { PORT = 3000 } = process.env;
 
@@ -17,29 +14,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(isURL),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), createUser);
-
-app.use(auth);
 app.use(routes);
 
 app.use(errors());
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
+  const { statusCode = SERVER_ERROR, message } = err;
+  res.status(statusCode).send({ message: statusCode === SERVER_ERROR ? 'На сервере произошла ошибка' : message });
   next();
 });
 
--- routes/app.js
+++ routes/app.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 const { usersRoutes } = require('./users');
 const { cardsRoutes } = require('./cards');
 const NotFoundError = require('../errors/not-found-error');
+const { login, createUser } = require('../controllers/users');
+const { isURL } = require('../helpers/regex');
+const auth = require('../middlewares/auth');
 
 const routes = express.Router();
 
+routes.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+routes.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(isURL),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
+
+routes.use(auth);
 routes.use('/users', usersRoutes);
 routes.use('/cards', cardsRoutes);
 routes.use('/*', (req, res, next) => {
